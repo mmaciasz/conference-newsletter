70c98a2eff24726a4cbf87103ceb2277662b9b22
student <student@localhost>
2016-05-29 18:05:40 +0200

    Project start

---
+++ src/main/java/pl/com/pollub/ConferenceNewsletterApplication.java
@@ -0,0 +1,12 @@
+package pl.com.pollub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceNewsletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceNewsletterApplication.class, args);
+	}
+}
