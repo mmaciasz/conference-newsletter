95da2708622b6914f878ed29f435772a44e89c99
Mateusz Maciasz <mateusz.maciasz@cgmpolska.pl>
2016-05-31 14:42:03 +0200

    Add database config + cron expressions

--- src/main/java/pl/com/pollub/task/Scheduler.java
+++ src/main/java/pl/com/pollub/task/Scheduler.java
@@ -6,10 +6,18 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CronTask {
+public class Scheduler {
 	
 	@Scheduled(cron = "${cronExpression}")
 	private void doNothing() {
 		System.out.println("Hello you " + new Date().toString());
+	}
+
+	/**
+	 * Task do odśmiecania pamięci raz dziennie.
+	 */
+	@Scheduled(cron = "${gc.cron.expresssion}")
+	private void gc() {
+		System.gc();
 	}
 }
