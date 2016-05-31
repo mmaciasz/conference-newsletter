c0b71c100afbc879f03a98a490f9790a13386469
Mateusz Maciasz <mateusz.maciasz@cgmpolska.pl>
2016-05-31 14:42:26 +0200

    Add database config + cron expresssions

--- src/main/java/pl/com/pollub/task/Scheduler.java
+++ src/main/java/pl/com/pollub/task/Scheduler.java
@@ -1,23 +1,54 @@
 package pl.com.pollub.task;
 
-import java.util.Date;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduler {
-	
-	@Scheduled(cron = "${cronExpression}")
-	private void doNothing() {
-		System.out.println("Hello you " + new Date().toString());
-	}
 
-	/**
-	 * Task do odśmiecania pamięci raz dziennie.
-	 */
-	@Scheduled(cron = "${gc.cron.expresssion}")
-	private void gc() {
-		System.gc();
-	}
+    private static final Logger log = LoggerFactory.getLogger(Scheduler.class);
+
+    /**
+     * Daily task
+     */
+    @Scheduled(cron = "${cron.daily.expression}")
+    private void dailyTask() {
+        log.info("Executiong daily task...");
+    }
+
+    /**
+     * Weekly task
+     */
+    @Scheduled(cron = "${cron.weekly.expression}")
+    private void weeklyTask() {
+        log.info("Executiong weekly task...");
+    }
+
+    /**
+     * Monthly task
+     */
+    @Scheduled(cron = "${cron.monthly.expression}")
+    private void monthlyTask() {
+        log.info("Executing monthly task...");
+    }
+
+    /**
+     * Quarter task
+     */
+    @Scheduled(cron = "${cron.quarter.expression}")
+    private void quarterTask() {
+        log.info("Executing quarter task...");
+    }
+
+    /**
+     * Daily garbage collection task.
+     */
+    @Scheduled(cron = "${cron.gc.expresssion}")
+    private void gc() {
+        log.info("Garbage collection started.");
+        System.gc();
+        log.info("Garbage collection ended.");
+    }
 }
