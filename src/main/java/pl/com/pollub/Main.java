d6aaac7a2bd274470fb5dba24c95647a844ed222
Mateusz Maciasz <mateusz.maciasz@cgmpolska.pl>
2016-05-31 13:17:33 +0200

    Banner + rename main class

--- src/main/java/pl/com/pollub/Main.java
+++ src/main/java/pl/com/pollub/Main.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"pl.com.pollub"})
 @EnableScheduling
-public class ConferenceNewsletterApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConferenceNewsletterApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 }
