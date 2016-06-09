f0c70589af5eca465d65f893e76d7ddc14c3bbe8
lajsek <andlis14@wp.pl>
2016-06-09 19:42:41 +0200

    Add properties to mail ( i cant send mail without it )

--- src/main/java/pl/com/webmail/MailSender.java
+++ src/main/java/pl/com/webmail/MailSender.java
@@ -35,6 +35,7 @@ public class MailSender {
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
         session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
