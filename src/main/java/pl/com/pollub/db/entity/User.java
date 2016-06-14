c6133fdbe8f316eef8d4de38bbccd5532b448c2e
lajsek <andlis14@wp.pl>
2016-06-14 20:52:54 +0200

    add user entity

---
+++ src/main/java/pl/com/pollub/db/entity/User.java
@@ -0,0 +1,52 @@
+package pl.com.pollub.db.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by kowal244 on 2016-06-14.
+ */
+
+@Entity
+@Table(name = "User")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+
+	private String email;
+	private int isActive;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(int isActive) {
+		this.isActive = isActive;
+	}
+
+	public User(){
+		
+	}
+	
+}
